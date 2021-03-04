5515e79fbeee40b0d84985efaffe2ce681d616ac
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2021-03-04 14:05:16 +0100

    Added XML Comments to Core interfaces

--- src/Core/src/Core/IEntry.cs
+++ src/Core/src/Core/IEntry.cs
@@ -1,7 +1,13 @@
 ﻿namespace Microsoft.Maui
 {
+	/// <summary>
+	/// Represents a View that is used for single-line text input.
+	/// </summary>
 	public interface IEntry : IView, IText
 	{
+		/// <summary>
+		/// Gets a value that indicates if the entry should visually obscure typed text.
+		/// </summary>
 		bool IsPassword { get; }
 	}
 }
--- src/Core/src/Core/ILabel.cs
+++ src/Core/src/Core/ILabel.cs
@@ -1,6 +1,10 @@
 namespace Microsoft.Maui
 {
+	/// <summary>
+	/// Represents a View that displays text.
+	/// </summary>
 	public interface ILabel : IView, IText, IFont
 	{
+
 	}
 }
