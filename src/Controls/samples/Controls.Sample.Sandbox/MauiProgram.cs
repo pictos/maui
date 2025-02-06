469a19be5de0bd435084960620a845a932ebc8ae
Pedro Jesus <pedrojesus.cefet@gmail.com>
2025-02-05 22:32:35 -0300

    sample

--- src/Controls/samples/Controls.Sample.Sandbox/MauiProgram.cs
+++ src/Controls/samples/Controls.Sample.Sandbox/MauiProgram.cs
@@ -1,9 +1,13 @@
-﻿namespace Maui.Controls.Sample;
+﻿using Microsoft.Maui.LifecycleEvents;
+using Microsoft.Maui.Platform;
+
+namespace Maui.Controls.Sample;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp() =>
-		MauiApp
+	public static MauiApp CreateMauiApp()
+	{
+		return MauiApp
 			.CreateBuilder()
 #if __ANDROID__ || __IOS__
 			.UseMauiMaps()
@@ -22,5 +26,31 @@ public static class MauiProgram
 				fonts.AddFont("SegoeUI-Italic.ttf", "Segoe UI Italic");
 				fonts.AddFont("SegoeUI-Bold-Italic.ttf", "Segoe UI Bold Italic");
 			})
+#if ANDROID
+			.ConfigureLifecycleEvents(static lifecycleBuilder =>
+			{
+				lifecycleBuilder.AddAndroid(static androidBuilder =>
+				{
+					androidBuilder.OnCreate(static (activity, _) =>
+					{
+						if (activity is not AndroidX.AppCompat.App.AppCompatActivity componentActivity)
+						{
+							System.Diagnostics.Trace.WriteLine($"Unable to Modify Android StatusBar On ModalPage: Activity {activity.LocalClassName} must be an {nameof(AndroidX.AppCompat.App.AppCompatActivity)}");
+							return;
+						}
+
+						if (componentActivity.GetFragmentManager() is not AndroidX.Fragment.App.FragmentManager fragmentManager)
+						{
+							System.Diagnostics.Trace.WriteLine($"Unable to Modify Android StatusBar On ModalPage: Unable to retrieve fragment manager from {nameof(AndroidX.AppCompat.App.AppCompatActivity)}");
+							return;
+						}
+
+						fragmentManager.RegisterFragmentLifecycleCallbacks(new FragmentLifecycleManager(), false);
+					});
+				});
+			})
+#endif
 			.Build();
+	}
+
 }
--- src/Controls/samples/Controls.Sample.Sandbox/Platforms/Android/MainActivity.cs
+++ src/Controls/samples/Controls.Sample.Sandbox/Platforms/Android/MainActivity.cs
@@ -1,6 +1,9 @@
 using Android.App;
+using Android.Content;
 using Android.Content.PM;
+using Android.OS;
 using Microsoft.Maui;
+using FragmentManager = AndroidX.Fragment.App.FragmentManager;
 
 namespace Maui.Controls.Sample.Platform
 {
@@ -13,5 +16,75 @@ namespace Maui.Controls.Sample.Platform
 		Categories = new[] { Android.Content.Intent.CategoryDefault })]
 	public class MainActivity : MauiAppCompatActivity
 	{
+	}
+}
+
+
+public sealed class FragmentLifecycleManager : FragmentManager.FragmentLifecycleCallbacks
+{
+
+	public override void OnFragmentAttached(FragmentManager fm, AndroidX.Fragment.App.Fragment f, Context context)
+	{
+		base.OnFragmentAttached(fm, f, context);
+	}
+
+	public override void OnFragmentCreated(FragmentManager fm, AndroidX.Fragment.App.Fragment f, Bundle? savedInstanceState)
+	{
+		base.OnFragmentCreated(fm, f, savedInstanceState);
+	}
+
+	public override void OnFragmentDestroyed(FragmentManager fm, AndroidX.Fragment.App.Fragment f)
+	{
+		base.OnFragmentDestroyed(fm, f);
+	}
+
+	public override void OnFragmentDetached(FragmentManager fm, AndroidX.Fragment.App.Fragment f)
+	{
+		base.OnFragmentDetached(fm, f);
+	}
+
+	public override void OnFragmentPaused(FragmentManager fm, AndroidX.Fragment.App.Fragment f)
+	{
+		base.OnFragmentPaused(fm, f);
+	}
+
+	public override void OnFragmentPreAttached(FragmentManager fm, AndroidX.Fragment.App.Fragment f, Context context)
+	{
+		base.OnFragmentPreAttached(fm, f, context);
+	}
+
+	public override void OnFragmentPreCreated(FragmentManager fm, AndroidX.Fragment.App.Fragment f, Bundle? savedInstanceState)
+	{
+		base.OnFragmentPreCreated(fm, f, savedInstanceState);
+	}
+
+	public override void OnFragmentResumed(FragmentManager fm, AndroidX.Fragment.App.Fragment f)
+	{
+		base.OnFragmentResumed(fm, f);
+	}
+
+	public override void OnFragmentSaveInstanceState(FragmentManager fm, AndroidX.Fragment.App.Fragment f, Bundle outState)
+	{
+		base.OnFragmentSaveInstanceState(fm, f, outState);
+	}
+
+	public override void OnFragmentStarted(FragmentManager fm, AndroidX.Fragment.App.Fragment f)
+	{
+		base.OnFragmentStarted(fm, f);
+	}
+
+	public override void OnFragmentStopped(FragmentManager fm, AndroidX.Fragment.App.Fragment f)
+	{
+		base.OnFragmentStopped(fm, f);
+	}
+
+	public override void OnFragmentViewCreated(FragmentManager fm, AndroidX.Fragment.App.Fragment f, Android.Views.View v, Bundle? savedInstanceState)
+	{
+		base.OnFragmentViewCreated(fm, f, v, savedInstanceState);
+	}
+
+	public override void OnFragmentViewDestroyed(FragmentManager fm, AndroidX.Fragment.App.Fragment f)
+	{
+		base.OnFragmentViewDestroyed(fm, f);
 	}
 }
